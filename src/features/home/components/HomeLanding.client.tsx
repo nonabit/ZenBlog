b38911af43356842751565c6f31d77570a498b43
ninebyte <oldmeatovo@gmail.com>
2026-02-24 21:50:20 +0800

    重写 HomeLanding 组装新的四区块布局

--- src/features/home/components/HomeLanding.client.tsx
+++ src/features/home/components/HomeLanding.client.tsx
@@ -1,14 +1,23 @@
 import type { HomeLandingProps } from '@/features/home/types';
 import HomeHeroSection from '@/features/home/sections/HomeHeroSection.client';
-import HomeBentoSection from '@/features/home/sections/HomeBentoSection.client';
 import HomeWritingSection from '@/features/home/sections/HomeWritingSection.client';
+import HomePhotographySection from '@/features/home/sections/HomePhotographySection.client';
+import HomeProjectsSection from '@/features/home/sections/HomeProjectsSection.client';
 
-export default function HomeLanding({ posts, lang, t }: HomeLandingProps) {
+export default function HomeLanding({ posts, photos, projects, lang, t }: HomeLandingProps) {
   return (
-    <div className="max-w-5xl mx-auto px-6 py-16 sm:py-24">
-      <HomeHeroSection t={t} />
-      <HomeBentoSection t={t} />
-      <HomeWritingSection posts={posts} lang={lang} t={t} />
+    <div className="mx-auto px-6 py-16 sm:py-24">
+      {/* Hero 和 Writing 区：窄宽度 640px */}
+      <div className="max-w-2xl mx-auto">
+        <HomeHeroSection t={t} />
+        <HomeWritingSection posts={posts} lang={lang} t={t} />
+      </div>
+
+      {/* Photography 和 Projects 区：宽宽度 960px */}
+      <div className="max-w-4xl mx-auto">
+        <HomePhotographySection photos={photos} lang={lang} t={t} />
+        <HomeProjectsSection projects={projects} lang={lang} t={t} />
+      </div>
     </div>
   );
 }
